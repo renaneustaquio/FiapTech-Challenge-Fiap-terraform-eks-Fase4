163c2267f856a0d03b30f4fb2e912fab1813134f
renan.ecm@hotmail.com <renan.ecm@hotmail.com>
2025-05-20 23:55:00 -0300

    no message

--- Terraform/variaveis.tf
+++ Terraform/variaveis.tf
@@ -29,7 +29,7 @@ variable "mercado_pago_access_token" {
 variable "aws_access_key_id" {
   type        = string
   description = "ID de chave de acesso da AWS"
-  default     = ""  
+  default     = ""
 }
 
 variable "aws_secret_access_key" {
@@ -41,7 +41,7 @@ variable "aws_secret_access_key" {
 variable "aws_session_token" {
   type        = string
   description = "Token da sessão de acesso da AWS"
-  default     = ""  
+  default     = ""
 }
 
 variable "aws_cognito_user_pool_id" {
